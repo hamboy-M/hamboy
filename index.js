fc8d888c2a0ed5a4d3fe5f6d23d8f55e647513ee
hamboy-M <kouta.syouta.0904@gmail.com>
2024-04-30 18:37:14 +0900

    ミズノはじゃんけんを作った

---
+++ index.js
@@ -0,0 +1,39 @@
+const choices=document.querySelector(".choices")
+const btn=document.querySelector(".btn")
+const enemy=document.querySelector(".enemy")
+const result=document.querySelector(".result")
+//グーチョキパーの配列
+const arry=["グー","チョキ","パー"]
+//ボタン要素が押されたらイベントを開始
+btn.addEventListener('click',()=>{
+    const random=Math.floor(Math.random()*arry.length)
+    enemy.value=arry[random]
+    enemy.value=arry[Math.floor(Math.random())]
+
+    //プレイヤーの選択肢と敵の選択肢の比較
+    if(choices.value==enemy.value){
+        result.textContent="あいこ"
+    }else{
+        if(choices.value=="グー"){
+            //自分がグーを出したとき
+        }
+        if(choices.value=="チョキ"){
+            //自分がチョキを出したとき
+        }
+        if(choices.value=="パー"){
+            //自分がパーを出したとき
+        }
+    }else{
+        if(choices.value=="グー"){
+            if(enemy.value=="チョキ"){
+                rusult
+            }
+        }
+
+
+
+
+
+
+    }
+})
