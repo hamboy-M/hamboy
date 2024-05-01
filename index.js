db9eaca8e155a9da294b3699c3a50e811809cc1b
hamboy-M <kouta.syouta.0904@gmail.com>
2024-05-01 17:42:12 +0900

    水野はじゃんけんゲームを作った

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const result=document.querySelector(".result")
 const arry=["グー","チョキ","パー"]
 //ボタン要素が押されたらイベントを開始
 btn.addEventListener('click',()=>{
-    const random=Math.floor(Math.random()*arry.length(3))
+    const random=Math.floor(Math.random()*arry.length)
     enemy.value=arry[random]
     enemy.value=arry[Math.floor(Math.random())]
 
